df22de1239c38f5a13a5042e43edc249049b159d
osomme <osomme@gmail.com>
2017-09-30 16:07:59 +0200

    Initial upload
    Slik er prosjektet i det jeg laster det opp for første gang. Artikkelsidene "SVG" og "Git" har enda ikke innhold.

---
+++ css/scripts.js
@@ -0,0 +1,42 @@
+window.onload = oppstart;
+
+function oppstart() {
+  setInterval(tidTilEksamen, 1000);
+  document.getElementById("hjemmeKnapp").onmouseover = gulHomeButton;
+  document.getElementById("hjemmeKnapp").onmouseout = svartHomeButton;
+}
+
+function gulHomeButton() {
+  document.getElementById("hjemmeKnapp").src = "css/homeorange.svg";
+}
+
+function svartHomeButton() {
+  document.getElementById("hjemmeKnapp").src = "css/home.svg";
+}
+
+function tidTilEksamen() {
+  var output = document.getElementById("tidtiloblig");
+  var naa = new Date();
+  var frist = new Date("10/18/2017 23:59");
+
+  var forskjell = frist.getTime() - naa.getTime();
+  var sekunder = Math.floor(forskjell / 1000);
+  var forskjellIDager = Math.floor(forskjell / (1000 * 60 * 60 * 24));
+  var forskjellIMinutter = Math.floor(sekunder / 60);
+  var forskjellITimer = Math.round((sekunder % 86400) / 3600);
+
+  if (!output) {
+    console.log("Timeren lastes ikke fordi outputelementet (tidtiloblig) ikke finnes på denne siden")
+  } else if (sekunder < 3600) {
+    if (sekunder <= 60) {
+      output.innerHTML = "Det er " + forskjellIDager +
+        " dager og " + forskjellIMinutter + " minutt igjen til fristen på oblig 3 går ut.";
+    } else {
+      output.innerHTML = "Det er " + forskjellIDager +
+        " dager og " + forskjellIMinutter + " minutter igjen til fristen på oblig 3 går ut.";
+    }
+  } else {
+    output.innerHTML = "Det er " + forskjellIDager +
+      " dager og " + forskjellITimer + " timer igjen til fristen på oblig 3 går ut.";
+  }
+}
